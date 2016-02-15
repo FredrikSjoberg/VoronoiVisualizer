71357555a895554f0e2aff9fcbd65afd0a6e5b6b
FredrikSjoberg <fredrik.sjoberg.swe@gmail.com>
2016-02-15 22:51:36 +0100

    updated for new non throwing Descartes API (voronoi)

--- VoronoiVisualizer/GameScene.swift
+++ VoronoiVisualizer/GameScene.swift
@@ -58,30 +58,25 @@ class GameScene: SKScene {
             let bounds = CGRect(x: 0, y: 0, width: sideSize, height: sideSize)
             let points = siteNodes.children.map{ $0.position }
             
-            do {
-                let voronoi = try Voronoi(points: points, bounds: bounds)
+            let voronoi = Voronoi(points: points, bounds: bounds)
+            
+            let edges = voronoi.voronoiEdges
+            edges.forEach{
+                let node = SKShapeNode()
+                let path = CGPathCreateMutable()
+                CGPathMoveToPoint(path, nil, $0.p0.x, $0.p0.y)
+                CGPathAddLineToPoint(path, nil, $0.p1.x, $0.p1.y)
+                node.path = path
+                edgeNodes.addChild(node)
                 
-                let edges = voronoi.voronoiEdges
-                edges.forEach{
-                    let node = SKShapeNode()
-                    let path = CGPathCreateMutable()
-                    CGPathMoveToPoint(path, nil, $0.p0.x, $0.p0.y)
-                    CGPathAddLineToPoint(path, nil, $0.p1.x, $0.p1.y)
-                    node.path = path
-                    edgeNodes.addChild(node)
-                    
-                    let vertex0 = SKShapeNode(circleOfRadius: 2)
-                    vertex0.position = $0.p0
-                    vertex0.fillColor = UIColor.redColor()
-                    let vertex1 = SKShapeNode(circleOfRadius: 2)
-                    vertex1.position = $0.p1
-                    vertex1.fillColor = UIColor.redColor()
-                    edgeNodes.addChild(vertex0)
-                    edgeNodes.addChild(vertex1)
-                }
-            }
-            catch {
-                print(error)
+                let vertex0 = SKShapeNode(circleOfRadius: 2)
+                vertex0.position = $0.p0
+                vertex0.fillColor = UIColor.redColor()
+                let vertex1 = SKShapeNode(circleOfRadius: 2)
+                vertex1.position = $0.p1
+                vertex1.fillColor = UIColor.redColor()
+                edgeNodes.addChild(vertex0)
+                edgeNodes.addChild(vertex1)
             }
             
             needsEdgeUpdate = false
